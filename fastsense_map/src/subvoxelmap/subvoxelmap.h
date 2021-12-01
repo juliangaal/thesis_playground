6dac7aee9ae537defd67f96803c2b5b0c2f4a3be
juliangaal <gjulian@uos.de>
2021-12-01 13:51:25 +0100

    hw-less fastsense map, initial testing successful

---
+++ fastsense_map/src/subvoxelmap/subvoxelmap.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "util.h"
+#include <iostream>
+#include <fmt/printf.h>
+#include <cassert>
+
+namespace map
+{
+
+class SubvoxelMap
+{
+public:
+    SubvoxelMap(int w, int h, int d, double res, int map_size)
+    : w(w), h(h), d(d), map(new double[h * w * d]), res(res), map_size(map_size)
+    {
+        assert(w > res);
+        init_map();
+    }
+
+    SubvoxelMap(int map_size, double res)
+    : w(static_cast<int>(map_size / res))
+    , h(static_cast<int>(map_size / res))
+    , d(static_cast<int>(map_size / res))
+    , map(new double[h * w * d])
+    , res(res)
+    , map_size(map_size)
+    {
+        assert(w > res);
+        init_map();
+    }
+
+    ~SubvoxelMap()
+    {
+        delete[] map;
+    }
+
+    SubvoxelMap& operator=(const SubvoxelMap&) = delete;
+    SubvoxelMap& operator=(SubvoxelMap&&) = delete;
+    SubvoxelMap(const SubvoxelMap&) = delete;
+    SubvoxelMap(SubvoxelMap&&) = delete;
+
+    double at_point(double x, double y, double z) const
+    {
+        util::Point<int> _3dindex = util::conv_3dpoint_3dindex(x, y, z, res);
+        return at_index(_3dindex.x, _3dindex.y, _3dindex.z);
+    }
+
+    double at_index(int x, int y, int z) const
+    {
+        if (!in_range(x, y, z))
+        {
+            throw std::runtime_error(fmt::format("Accessing subvoxelmap @ invalid location: accessed @ ({}/{}/{}) with size {}\n", x, y, z, _size()));
+        }
+        int i = util::conv_3dindex_1dindex(x, y, z, w, d);
+        return map[i];
+    }
+
+    // TODO auslagern
+    void insert(double x, double y, double z, double val)
+    {
+        util::Point<int> _3dindex = util::conv_3dpoint_3dindex(x, y, z, res);
+        if (!in_range(_3dindex.x, _3dindex.y, _3dindex.z))
+        {
+            throw std::runtime_error(fmt::format("Accessing subvoxelmap @ invalid location: accessed @ ({}/{}/{}) with size {}\n", x, y, z, _size()));
+        }
+        int i = util::conv_3dindex_1dindex(_3dindex.x, _3dindex.y, _3dindex.z, w, d);
+        map[i] = val;
+    }
+
+    void clear()
+    {
+        init_map();
+    }
+
+    int _size() const
+    {
+        return h * w * d;
+    }
+
+    int _map_size() const
+    {
+        return map_size;
+    }
+
+    double _res() const
+    {
+        return res;
+    }
+
+    int _h() const
+    {
+        return h;
+    }
+
+    int _w() const
+    {
+        return w;
+    }
+
+    int _d() const
+    {
+        return d;
+    }
+
+private:
+    void init_map()
+    {
+        std::fill_n(map, _size(), NAN);
+    }
+
+    bool in_range(int x, int y, int z) const
+    {
+        return x >= 0 && x < w && y >= 0 && y < h && z >= 0 && z < d;
+    }
+
+    int w;
+    int h;
+    int d;
+    double *map;
+    double res;
+    int map_size;
+};
+
+} // end namespace map
---
+++ fastsense_map/src/test.cpp
@@ -0,0 +1,181 @@
+/**
+ * @author Steffen Hinderink
+ * @author Juri Vana
+ */
+
+#include "local_map.h"
+#include <catch2/catch_test_macros.hpp>
+#include <fmt/printf.h>
+
+using namespace fastsense::map;
+using Eigen::Vector3i;
+
+constexpr int DEFAULT_VALUE = 4;
+constexpr int DEFAULT_WEIGHT = 6;
+
+TEST_CASE("Map", "[Map]")
+{
+    fmt::print("Testing Map");
+    std::shared_ptr<GlobalMap> gm_ptr = std::make_shared<GlobalMap>("MapTest.h5", DEFAULT_VALUE, DEFAULT_WEIGHT);
+    LocalMap localMap{5, 5, 5, gm_ptr};
+    
+    /*
+     * Write some tsdf values and weights into the local map,
+     * that will be written to the file as one chunk (-1_0_0)
+     *
+     *    y
+     *    ^
+     *  4 | .   .   .   . | .   .
+     *    | Chunk -1_0_0  | Chunk 0_0_0
+     *  3 | .   .   .   . | .   .
+     *    |               |
+     *  2 | .   .  p0  p1 | .   .
+     *    |               |
+     *  1 | .   .  p2  p3 | .   .
+     *    |               |
+     *  0 | .   .  p4  p5 | .   .
+     *    | --------------+------
+     * -1 | .   .   .   . | .   .
+     *    | Chunk -1_-1_0 | Chunk 0_-1_0
+     * -2 | .   .   .   . | .   .
+     *    +-----------------------> x
+     *   / -4  -3  -2  -1   0   1
+     * z=0
+     */
+    TSDFEntry p0(0, 0);
+    TSDFEntry p1(1, 1);
+    TSDFEntry p2(2, 1);
+    TSDFEntry p3(3, 2);
+    TSDFEntry p4(4, 3);
+    TSDFEntry p5(5, 5);
+    localMap.value(-2, 2, 0) = p0;
+    localMap.value(-1, 2, 0) = p1;
+    localMap.value(-2, 1, 0) = p2;
+    localMap.value(-1, 1, 0) = p3;
+    localMap.value(-2, 0, 0) = p4;
+    localMap.value(-1, 0, 0) = p5;
+    
+    // test getter
+    CHECK(localMap.get_pos() == Vector3i(0, 0, 0));
+    CHECK(localMap.get_size() == Vector3i(5, 5, 5));
+    CHECK(localMap.get_offset() == Vector3i(2, 2, 2));
+    
+    // test in_bounds
+    CHECK(localMap.in_bounds(0, 2, -2));
+    CHECK(!localMap.in_bounds(22, 0, 0));
+    // test default values
+    CHECK(localMap.value(0, 0, 0).value() == DEFAULT_VALUE);
+    CHECK(localMap.value(0, 0, 0).weight() == DEFAULT_WEIGHT);
+    // test value access
+    CHECK(localMap.value(-1, 2, 0).value() == 1);
+    CHECK(localMap.value(-1, 2, 0).weight() == 1);
+    
+    // ==================== shift ====================
+    // shift so that the chunk gets unloaded
+    // Each shift can only cover an area of one Map size
+    localMap.shift(Vector3i( 5, 0, 0));
+    localMap.shift(Vector3i(10, 0, 0));
+    localMap.shift(Vector3i(15, 0, 0));
+    localMap.shift(Vector3i(20, 0, 0));
+    localMap.shift(Vector3i(24, 0, 0));
+    
+    CHECK(localMap.get_pos() == Vector3i(24, 0, 0));
+    CHECK(localMap.get_size() == Vector3i(5, 5, 5));
+    CHECK(localMap.get_offset() == Vector3i(26 % 5, 2, 2));
+    
+    // test in_bounds
+    CHECK(!localMap.in_bounds(0, 2, -2));
+    CHECK(localMap.in_bounds(22, 0, 0));
+    // test values
+    CHECK(localMap.value(24, 0, 0).value() == DEFAULT_VALUE);
+    CHECK(localMap.value(24, 0, 0).weight() == DEFAULT_WEIGHT);
+    
+    // ==================== shift directions ====================
+    localMap.value(24, 0, 0) = TSDFEntry(24, 0);
+    
+    localMap.shift(Vector3i(24, 5, 0));
+    localMap.value(24, 5, 0) = TSDFEntry(24, 5);
+    
+    localMap.shift(Vector3i(19, 5, 0));
+    localMap.value(19, 5, 0) = TSDFEntry(19, 5);
+    
+    localMap.shift(Vector3i(19, 0, 0));
+    localMap.value(19, 0, 0) = TSDFEntry(19, 0);
+    
+    localMap.shift(Vector3i(24, 0, 0));
+    CHECK(localMap.value(24, 0, 0).value() == 24);
+    CHECK(localMap.value(24, 0, 0).weight() == 0);
+    
+    localMap.shift(Vector3i(19, 0, 0));
+    CHECK(localMap.value(19, 0, 0).value() == 19);
+    CHECK(localMap.value(19, 0, 0).weight() == 0);
+    localMap.shift(Vector3i(24, 5, 0));
+    CHECK(localMap.value(24, 5, 0).value() == 24);
+    CHECK(localMap.value(24, 5, 0).weight() == 5);
+    localMap.shift(Vector3i(19, 5, 0));
+    CHECK(localMap.value(19, 5, 0).value() == 19);
+    CHECK(localMap.value(19, 5, 0).weight() == 5);
+    localMap.shift(Vector3i(24, 0, 0));
+    CHECK(localMap.value(24, 0, 0).value() == 24);
+    CHECK(localMap.value(24, 0, 0).weight() == 0);
+    
+    // ==================== shift back ====================
+    localMap.shift(Vector3i(19, 0, 0));
+    localMap.shift(Vector3i(14, 0, 0));
+    localMap.shift(Vector3i( 9, 0, 0));
+    localMap.shift(Vector3i( 4, 0, 0));
+    localMap.shift(Vector3i( 0, 0, 0));
+    
+    // test return correct
+    CHECK(localMap.get_pos() == Vector3i(0, 0, 0));
+    CHECK(localMap.get_size() == Vector3i(5, 5, 5));
+    CHECK(localMap.get_offset() == Vector3i(2, 2, 2));
+    
+    // test in_bounds
+    CHECK(localMap.in_bounds(0, 2, -2));
+    CHECK(!localMap.in_bounds(22, 0, 0));
+    
+    CHECK(localMap.value(0, 0, 0).value() == DEFAULT_VALUE);
+    CHECK(localMap.value(0, 0, 0).weight() == DEFAULT_WEIGHT);
+    CHECK(localMap.value(-1, 2, 0).value() == 1);
+    CHECK(localMap.value(-1, 2, 0).weight() == 1);
+    
+    // ==================== kernel ====================
+    
+    // Manipulate the map by applying a kernel that doubles the tsdf values and halfes the weights and shifting
+//    auto q = FPGAManager::create_command_queue();
+//    LocalMapTestKernel krnl{q};
+//    krnl.run(localMap);
+//    krnl.waitComplete();
+//
+//     Test manipulated map
+//    CHECK(localMap.value(0, 0, 0).value() == DEFAULT_VALUE * 2);
+//    CHECK(localMap.value(0, 0, 0).weight() == DEFAULT_WEIGHT / 2);
+//    CHECK(localMap.value(-1, 2, 0).value() == 2);
+//    CHECK(localMap.value(-1, 2, 0).weight() == 0);
+    
+    // Test persistent storage in HDF5 file
+//    localMap.write_back();
+    
+//    HighFive::File f("MapTest.h5", HighFive::File::OpenOrCreate);
+//    HighFive::Group g = f.getGroup("/map");
+//    HighFive::DataSet d = g.getDataSet("-1_0_0");
+//    std::vector<TSDFEntry::RawType> chunk;
+//    d.read(chunk);
+//
+//    constexpr int CHUNK_SIZE = GlobalMap::CHUNK_SIZE;
+//
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 2)]).value() == 0 * 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 2)]).value() == 1 * 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 1)]).value() == 2 * 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 1)]).value() == 3 * 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 0)]).value() == 4 * 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 0)]).value() == 5 * 2);
+//
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 2)]).weight() == 0 / 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 2)]).weight() == 1 / 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 1)]).weight() == 1 / 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 1)]).weight() == 2 / 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 2) + CHUNK_SIZE * 0)]).weight() == 3 / 2);
+//    CHECK(TSDFEntry(chunk[(CHUNK_SIZE * CHUNK_SIZE * (CHUNK_SIZE - 1) + CHUNK_SIZE * 0)]).weight() == 5 / 2);
+}
