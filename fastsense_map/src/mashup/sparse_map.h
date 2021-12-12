8450deb30b94f35607f6c2904c92ac12ff840645
Julian Gaal <juliangaal@protonmail.com>
2021-12-12 14:57:34 +0100

    implement 3d sparsemap and test in accordance with 1d test

---
+++ fastsense_map/src/mashup/sparse_map.h
@@ -0,0 +1,226 @@
+#pragma once
+
+/**
+  * @file SparseSubMap.h
+  * @author julian 
+  * @date 12/11/21
+ */
+
+#include "subvoxelmap/util.h"
+#include "util/point.h"
+
+struct SparseSubMap
+{
+    SparseSubMap(int size, int res, const int& default_val)
+    : h_(size / res)
+    , w_(size / res)
+    , d_(size / res)
+    , data_(new int[h_ * w_ * d_])
+    , res_(res)
+    , default_val_(default_val)
+    , n_occupied_(0)
+    {
+        std::fill_n(data_, h_ * w_ * d_, default_val_);
+    }
+    
+    ~SparseSubMap()
+    {
+        delete[] data_;
+    }
+    
+    void insert_val(int x, int y, int z, int val)
+    {
+        data_[util::conv_3dpoint_1dindex(x, y, z, res_, w_, d_)] = val;
+        n_occupied_++;
+    }
+    
+    inline const int& at(int x, int y, int z) const
+    {
+        return data_[util::conv_3dpoint_1dindex(x, y, z, res_, w_, d_)];
+    }
+    
+    void remove_val(int x, int y, int z)
+    {
+        data_[util::conv_3dpoint_1dindex(x, y, z, res_, w_, d_)] = default_val_;
+        n_occupied_--;
+    }
+    
+    inline bool empty() const
+    {
+        return n_occupied_ == 0;
+    }
+    
+    inline int n_occupied() const
+    {
+        return n_occupied_;
+    }
+    
+    inline int n_elems() const
+    {
+        return h_ * w_ * d_;
+    }
+    
+    int h_;
+    int w_;
+    int d_;
+    int* data_;
+    int res_;
+    const int& default_val_;
+    int n_occupied_;
+};
+
+struct SparseMap
+{
+    SparseMap(unsigned int size, int res, int subvoxel_res, int default_val)
+    : h_(static_cast<int>(size % 2 == 1 ? (size + 1) / res : (size / res)))
+    , w_(h_)
+    , d_(h_)
+    , data_(new SparseSubMap*[h_ * w_ * d_])
+    , offset_(static_cast<int>(static_cast<int>(size % 2 == 1 ? (size + 1) / 2 : size / 2)))
+    , res_(res)
+    , subvoxel_res_(subvoxel_res)
+    , default_val_(default_val)
+    {
+        std::fill_n(data_, h_ * w_ * d_, nullptr);
+    }
+
+    ~SparseMap() {
+        for (int i = 0; i < h_ * w_ * d_; ++i)
+        {
+            delete data_[i];
+        }
+        delete[] data_;
+    }
+
+    void init_subvoxelmap(int index) const
+    {
+        if (subvoxel_unitialized(index)) {
+            data_[index] = new SparseSubMap(res_, subvoxel_res_, default_val_);
+        }
+    }
+
+    inline bool subvoxel_unitialized(int x) const
+    {
+        return data_[x] == nullptr;
+    }
+
+    void insert(int x, int y, int z, int val, bool apply_offset = false) const
+    {
+        if (!in_bounds(x, y, z, apply_offset))
+        {
+            throw std::out_of_range(fmt::format("Out of range (voxelmap, insert): w/o offset {}, val: {}", x, val));
+        }
+
+        if (apply_offset)
+        {
+            x += offset_;
+            y += offset_;
+            z += offset_;
+        }
+
+        int index = util::conv_3dpoint_1dindex(x, y, z, res_, w_, d_);;
+        init_subvoxelmap(index);
+        insert_into_subvoxel(index, x, y, z, val);
+    }
+
+    const SparseSubMap* sparse_submap(int x, int y, int z) const
+    {
+        if (!in_bounds(x, y, z, false))
+        {
+            throw std::out_of_range(fmt::format("Can't access subvoxelmap out of range (voxelmap, subvoxelmap): {}", x));
+        }
+
+        return sparse_submap_unchecked(x, y, z);
+    }
+
+    const SparseSubMap* sparse_submap_unchecked(int x, int y, int z) const
+    {
+        return data_[util::conv_3dpoint_1dindex(x, y, z, res_, w_, d_)];
+    }
+
+    void insert_into_subvoxel(int index, int x, int y, int z, int val) const
+    {
+        // values here are always > 0 null, no need to specifically handle neg numbers and modulo
+        auto sub_x = (x % res_) / subvoxel_res_;
+        auto sub_y = (y % res_) / subvoxel_res_;
+        auto sub_z = (z % res_) / subvoxel_res_;
+        data_[index]->insert_val(sub_x, sub_y, sub_z, val);
+    }
+
+    const int& val_in_subvoxel(int x, int y, int z, bool apply_offset = false) const
+    {
+        if (!in_bounds(x, y, z, apply_offset))
+        {
+            throw std::out_of_range(fmt::format("Out of range (voxelmap, val_in_subvoxel): {}", x));
+        }
+
+        if (apply_offset)
+        {
+            x += offset_;
+            y += offset_;
+            z += offset_;
+        }
+
+        // offset already applied and in_bounds, so unchecked is safe
+        auto* subvoxel = sparse_submap_unchecked(x, y, z);
+        if (subvoxel == nullptr)
+        {
+            return default_val_;
+        }
+
+        // values here are always >0 null, no need to specifically handle neg numbers and modulo
+        auto sub_x = (x % res_) / subvoxel_res_;
+        auto sub_y = (y % res_) / subvoxel_res_;
+        auto sub_z = (z % res_) / subvoxel_res_;
+        return subvoxel->at(sub_x, sub_y, sub_z);
+    }
+
+    bool in_bounds(int x, int y, int z, bool offset_applied = false) const
+    {
+        if (offset_applied)
+        {
+            bool x_in_bounds = false;
+            if (x < 0)
+            {
+                x_in_bounds = std::abs(x) <= offset_;
+            }
+            else
+            {
+                x_in_bounds = x < offset_;
+            }
+
+            bool y_in_bounds = false;
+            if (y < 0)
+            {
+                y_in_bounds = std::abs(y) <= offset_;
+            }
+            else
+            {
+                y_in_bounds = y < offset_;
+            }
+
+            bool z_in_bounds = false;
+            if (z < 0)
+            {
+                z_in_bounds = std::abs(z) <= offset_;
+            }
+            else
+            {
+                z_in_bounds = z < offset_;
+            }
+
+            return x_in_bounds && y_in_bounds && z_in_bounds;
+        }
+
+        return x < (h_ * res_) && y < (h_ * res_) && z < (h_ * res_);
+    }
+
+    int h_;
+    int w_;
+    int d_;
+    SparseSubMap** data_;
+    int offset_;
+    int res_;
+    int subvoxel_res_;
+    int default_val_;
+};
--- fastsense_map/src/mashup/test.cpp
+++ fastsense_map/src/mashup/test.cpp
@@ -7,10 +7,11 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <fmt/printf.h>
-#include "sparse_sub_map.h"
+#include "sparse_map.h"
 
 TEST_CASE("SparseSubMap", "[SparseSubMap]")
 {
+    fmt::print("Testing SparseSubMap\n");
     {
         int size = 5;
         int default_val = -999;
@@ -89,5 +90,166 @@ TEST_CASE("SparseSubMap", "[SparseSubMap]")
         }
         
         REQUIRE(map.n_occupied() == 0);
+    }
+}
+
+TEST_CASE("VoxelMap", "[VoxelMap]")
+{
+    fmt::print("Testing SparseMap\n");
+
+    {
+        int default_val = -999;
+        SparseMap map(10, 1, 1, default_val);
+        REQUIRE(map.h_ == 10);
+        REQUIRE(map.w_ == 10);
+        REQUIRE(map.d_ == 10);
+        REQUIRE(map.offset_ == 5);
+
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == default_val);
+                }
+            }
+        }
+
+        int insert_val = 1;
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    map.insert(x, y, z, insert_val, true);
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == insert_val);
+                }
+            }
+        }
+
+        REQUIRE_THROWS(map.insert(-6, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -6, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -6, 1, true));
+        REQUIRE_THROWS(map.insert(-8, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -8, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -8, 1, true));
+        REQUIRE_THROWS(map.insert(5, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 5, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 5, 1, true));
+        REQUIRE_THROWS(map.insert(7, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 7, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 7, 1, true));
+    }
+
+    {
+        int default_val = -999;
+        SparseMap map(11, 2, 1, default_val);
+        REQUIRE(map.h_ == 6);
+        REQUIRE(map.w_ == 6);
+        REQUIRE(map.d_ == 6);
+        REQUIRE(map.offset_ == 6);
+
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == default_val);
+                }
+            }
+        }
+
+        int insert_val = 1;
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    map.insert(x, y, z, insert_val, true);
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == insert_val);
+                }
+            }
+        }
+
+        REQUIRE_THROWS(map.insert(-7, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -7, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -7, 1, true));
+        REQUIRE_THROWS(map.insert(-8, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -8, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -8, 1, true));
+        REQUIRE_THROWS(map.insert(6, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 6, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 6, 1, true));
+        REQUIRE_THROWS(map.insert(7, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 7, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 7, 1, true));
+    }
+
+    {
+        int default_val = -999;
+        SparseMap map(12, 4, 1, default_val);
+        REQUIRE(map.h_ == 3);
+        REQUIRE(map.w_ == 3);
+        REQUIRE(map.d_ == 3);
+        REQUIRE(map.offset_ == 6);
+
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == default_val);
+                }
+            }
+        }
+
+        int insert_val = 1;
+        for (int i = 0; i < map.h_ * map.res_; ++i)
+        {
+            for (int j = 0; j < map.w_ * map.res_; ++j)
+            {
+                for (int k = 0; k < map.d_ * map.res_; ++k)
+                {
+                    int x = i - map.offset_;
+                    int y = j - map.offset_;
+                    int z = k - map.offset_;
+                    map.insert(x, y, z, insert_val, true);
+                    REQUIRE(map.val_in_subvoxel(x, y, z, true) == insert_val);
+                }
+            }
+        }
+
+        REQUIRE_THROWS(map.insert(-7, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -7, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -7, 1, true));
+        REQUIRE_THROWS(map.insert(-8, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, -8, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, -8, 1, true));
+        REQUIRE_THROWS(map.insert(6, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 6, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 6, 1, true));
+        REQUIRE_THROWS(map.insert(7, 0, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 7, 0, 1, true));
+        REQUIRE_THROWS(map.insert(0, 0, 7, 1, true));
     }
 }
